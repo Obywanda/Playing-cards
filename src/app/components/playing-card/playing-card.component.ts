027242d430a56280243d3d317566ee336916701a
Obywanda <m.bourgeois.miranda@gmail.com>
2024-11-30 12:43:55 +0100

    Création de component playing-card + inputs

---
+++ src/app/components/playing-card/playing-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Monster } from '../../models/monster.model';
+
+@Component({
+  selector: 'app-playing-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './playing-card.component.html',
+  styleUrl: './playing-card.component.css'
+})
+export class PlayingCardComponent {
+
+  
+  //Le décorateur @Input() permet d'insceret une valeur en entrée
+  @Input({
+    
+  }
+  ) monster: Monster = new Monster()
+
+ }
---
+++ src/app/models/monster.model.ts
@@ -0,0 +1,9 @@
+export class Monster {
+    //Création des attribues pour le composant Playing-card
+    name: string = "My Monster";
+    hp: number = 40;
+    figureCaption: string = "N°001 Monster";
+    attackName: string = "Geo Impact";
+    attackStrenght: number = 60;
+    attacksDescription: string = "This is a long description of a monster attack. Probably something to do with electricity"
+}
