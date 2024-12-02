3e6cdfb9ea5410a165da37690905ee80bf482359
Obywanda <m.bourgeois.miranda@gmail.com>
2024-12-02 16:26:10 +0100

    Ajout du component search-bar + ajout des input / output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PlayingCardComponent } from "./components/playing-card/playing-card.component";
 import { Monster } from './models/monster.model';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 
 @Component({
@@ -8,16 +9,22 @@ import { Monster } from './models/monster.model';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [PlayingCardComponent],
+  imports: [PlayingCardComponent, SearchBarComponent],
 })
 export class AppComponent {
 
   monster1!: Monster;
+  count: number = 0;
+  search = "";
 
   constructor() {
     this.monster1 = new Monster();
     this.monster1.name = "Pik";
     this.monster1.hp = 40;
     this.monster1.figureCaption ="N°002 Pik";
+  }
+
+  increaseCount() {
+    this.count++;
   }
 }
--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -13,7 +13,10 @@ export class PlayingCardComponent {
   
   //Le décorateur @Input() permet d'insceret une valeur en entrée
   @Input({
-    
+    //parametre input
+    //required: true ; 
+    //alias:'my-monster' ;
+    //transform: (value: Monster) => {value.hp = value.hp /2; return value}
   }
   ) monster: Monster = new Monster()
 
